ec6cdc5f969cfcfec69dc2a4d1b0f0392bec6f2d
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2021-12-31 14:56:10 -0800

    add

---
+++ src/calculatorApp/Calculator.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Calculator.css";
+
+function Calculator() {
+  return (
+    <div className="container">
+      <div className="result"></div>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>x</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+      <div>-</div>
+      <div>7</div>
+      <div>8</div>
+      <div>9</div>
+      <div>+</div>
+      <div>0</div>
+      <div>.</div>
+      <div>=</div>
+      <div>/</div>
+      <div className="clear">Clear</div>
+    </div>
+  );
+}
+export default Calculator;
