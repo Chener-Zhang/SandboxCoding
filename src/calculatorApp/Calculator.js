5df4dc648fcbe828a3e994c9def60c5bfdf28596
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-01-01 11:40:43 -0800

    add fun

--- src/calculatorApp/Calculator.js
+++ src/calculatorApp/Calculator.js
@@ -2,23 +2,34 @@ import React, { useState } from "react";
 import "./Calculator.css";
 
 function Calculator() {
-  const [counter, setCounter] = useState(0);
+  const [expressionString, setExpressionString] = useState("");
 
   function onClickHandler(e) {
-    switch (e.target.innerText) {
-      case "+":
-        setCounter(counter + 1);
+    const value = e.target.innerText;
+    switch (value) {
+      case "Clear":
+        setExpressionString("");
+        break;
+      case "=":
+        setExpressionString(eval(expressionString));
+        break;
+      case "0":
+        if (expressionString.length !== 0) {
+          setExpressionString(expressionString + value);
+        }
+        break;
       default:
-        return null;
+        setExpressionString(expressionString + value);
     }
   }
+
   return (
     <div className="container">
-      <div className="result">{counter}</div>
+      <div className="result">{expressionString}</div>
       <div onClick={(e) => onClickHandler(e)}>1</div>
       <div onClick={(e) => onClickHandler(e)}>2</div>
       <div onClick={(e) => onClickHandler(e)}>3</div>
-      <div onClick={(e) => onClickHandler(e)}>x</div>
+      <div onClick={(e) => onClickHandler(e)}>*</div>
       <div onClick={(e) => onClickHandler(e)}>4</div>
       <div onClick={(e) => onClickHandler(e)}>5</div>
       <div onClick={(e) => onClickHandler(e)}>6</div>
