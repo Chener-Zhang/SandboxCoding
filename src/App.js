703e29a6fb25bea9e71b6375a576e80968c88a1a
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-01-16 10:51:22 -0800

    add test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import "./styles.css";
-import TimeCounter from "./TimeCounter/TimeCounter";
 
 export default function App() {
-  return (
-    <div className="App">
-      <TimeCounter />
-    </div>
-  );
+  return <div className="App"></div>;
 }
