f80b57152a9bd0e16a6d682d0db32c0daeb39650
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-01-17 13:23:07 -0800

    clear

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import "./styles.css";
-import TimeCounter from "./TimeCounter/TimeCounter";
 
 export default function App() {
   return <div className="App"></div>;
