55af6bad07e09e8e325bdc9350aed27662802269
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-01-27 11:01:58 -0800

    dummy api search feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
 import CalendarPicker from "./CalendarPicker";
 import InterviewQuestion from "./InterviewQuestion";
+import DummyAPI from "./DummyAPI";
 export default function App() {
   return (
     <>
-      <InterviewQuestion />
+      {/* <InterviewQuestion /> */}
+      <DummyAPI />
     </>
   );
 }
---
+++ src/DummyAPI.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+export default function DummyAPI() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    axios
+      .get("https://dummy.restapiexample.com/api/v1/employees")
+      .then((res) => setData(res.data.data))
+      .catch((errer) => console.log(errer));
+  }, []);
+
+  return (
+    <>
+      <input value={search} onChange={(e) => setSearch(e.target.value)} />
+      <h3>{search}</h3>
+      {data && (
+        <ul>
+          {data
+            .filter((item) => {
+              return item.employee_name.toLowerCase().includes(search);
+            })
+            .map((item) => {
+              return <li key={item.key}>{item.employee_name}</li>;
+            })}
+        </ul>
+      )}
+    </>
+  );
+}
