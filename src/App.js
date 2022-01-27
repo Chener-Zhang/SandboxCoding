f134a4ccd44c2e82aee9c37f81458b21daeb4bf4
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-01-26 21:12:26 -0800

    ChangeTheme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
 import "./styles.css";
+import { ThemeProvider } from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import { useState } from "react";
 
 export default function App() {
-  return <div className="App"></div>;
+  const [theme, setTheme] = useState("light");
+
+  const light = {
+    body: "#E2E2E2"
+  };
+  const dark = {
+    body: "#363537"
+  };
+  
+  const GlobalStyle = createGlobalStyle`
+  body{
+    background: ${({ theme }) => theme.body};
+  }
+  `c
+
+  return (
+    <ThemeProvider theme={theme === "light" ? dark : light}>
+      <GlobalStyle />
+      <button
+        onClick={() => {
+          if (theme === "light") {
+            setTheme("dark");
+          } else {
+            setTheme("light");
+          }
+        }}
+      >
+        change
+      </button>
+    </ThemeProvider>
+  );
 }
