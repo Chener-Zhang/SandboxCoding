dca8c540519675eba4b819aa0ff49305e8562a19
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-01-27 09:08:38 -0800

    Add the theme toggle

--- src/App.js
+++ src/App.js
@@ -1,38 +1,10 @@
 import "./styles.css";
-import { ThemeProvider } from "styled-components";
-import { createGlobalStyle } from "styled-components";
-import { useState } from "react";
+import ThemeToggle from "./ThemeToggle";
 
 export default function App() {
-  const [theme, setTheme] = useState("light");
-
-  const light = {
-    body: "#E2E2E2"
-  };
-  const dark = {
-    body: "#363537"
-  };
-
-  const GlobalStyle = createGlobalStyle`
-  body{
-    background: ${({ theme }) => theme.body};
-  }
-  `;
-
   return (
-    <ThemeProvider theme={theme === "light" ? dark : light}>
-      <GlobalStyle />
-      <button
-        onClick={() => {
-          if (theme === "light") {
-            setTheme("dark");
-          } else {
-            setTheme("light");
-          }
-        }}
-      >
-        change
-      </button>
-    </ThemeProvider>
+    <>
+      <ThemeToggle />
+    </>
   );
 }
---
+++ src/ThemeToggle.js
@@ -0,0 +1,36 @@
+import { ThemeProvider } from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import { useState } from "react";
+export default function ThemeToggle() {
+  const [theme, setTheme] = useState("light");
+
+  const light = {
+    body: "#FFFFFF"
+  };
+  const dark = {
+    body: "#000000"
+  };
+  const GlobleStyle = createGlobalStyle`
+
+  body{
+    background-color:${({ theme }) => theme.body}
+  }
+  `;
+
+  return (
+    <ThemeProvider theme={theme === "light" ? light : dark}>
+      <GlobleStyle />
+      <button
+        onClick={() => {
+          if (theme === "light") {
+            setTheme("dark");
+          } else {
+            setTheme("light");
+          }
+        }}
+      >
+        Change
+      </button>
+    </ThemeProvider>
+  );
+}
