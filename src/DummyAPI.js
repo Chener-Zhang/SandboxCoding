235798b0ce2322f084bf4dcf1e4a0a61a1ee393f
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-01-27 17:50:45 -0800

    Pagenation

--- src/DummyAPI.js
+++ src/DummyAPI.js
@@ -4,13 +4,19 @@ import { useEffect, useState } from "react";
 export default function DummyAPI() {
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1);
+  const [pageMax, setPageMax] = useState(0);
+  const dataPerPage = 5;
 
   useEffect(() => {
     //then((res) => setData(res.data.data)).slice(0,10)
     axios
       .get("https://dummy.restapiexample.com/api/v1/employees")
-      .then((res) => setData(res.data.data))
+      .then((res) => {
+        setData(res.data.data);
+        const page = Math.ceil(res.data.data.length / dataPerPage);
+        setPageMax(page);
+      })
       .catch((errer) => console.log(errer));
   }, []);
 
@@ -24,11 +30,34 @@ export default function DummyAPI() {
             .filter((item) => {
               return item.employee_name.toLowerCase().includes(search);
             })
-            .map((item) => {
-              return <li key={item.key}>{item.employee_name}</li>;
+            .map((item, index) => {
+              if (page === Math.ceil(index / dataPerPage)) {
+                return <li key={item.key}>{item.employee_name}</li>;
+              }
             })}
         </ul>
       )}
+      <div>
+        <h2>{page}</h2>
+        <button
+          onClick={() => {
+            if (page !== 0) {
+              setPage(page - 1);
+            }
+          }}
+        >
+          previous
+        </button>
+        <button
+          onClick={() => {
+            if (page < pageMax) {
+              setPage(page + 1);
+            }
+          }}
+        >
+          next
+        </button>
+      </div>
     </>
   );
 }
